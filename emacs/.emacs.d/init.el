1e6b108a391bc0e3f6183da1593992fce7dbc006
Brian Collison <brian@briancollison.com>
2020-11-04 18:46:08 -0800

    fixing fonts for mac

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,6 +18,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/Apps/MobileOrg/vgi.org" "~/Dropbox/Apps/MobileOrg/home.org" "~/Dropbox/Apps/MobileOrg/index.org")))
  '(package-selected-packages
    (quote
     (org-journal yard-mode yaml-mode which-key wgrep web-mode use-package synosaurus solarized-theme smex slim-mode scss-mode scala-mode sbt-mode ruby-end rspec-mode rainbow-delimiters python-mode py-autopep8 proof-general projectile-rails paredit org-bullets multi-term minions less-css-mode instapaper helpful haskell-mode haml-mode graphviz-dot-mode go-errcheck gnuplot forge flycheck-package flx evil-surround evil-org evil-mu4e evil-magit engine-mode elpy dumb-jump dired-open dired-hide-dotfiles diff-hl deft counsel company-jedi company-go company-coq coffee-mode chruby auto-compile ag))))
@@ -26,4 +29,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-code-face ((t nil))))
+ '(fixed-pitch ((t (:family "Inconsolata" :height 70))))
+ '(markdown-code-face ((t nil)))
+ '(variable-pitch ((t (:family "Libre Baskerville" :height 70 :weight thin)))))
