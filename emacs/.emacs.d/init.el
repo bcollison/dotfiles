31674d0a60007b50c5a5384218bb55477883d746
Brian Collison <brian@verve.com>
2021-02-08 07:17:37 -0800

    mu4e, elfeed, java

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,6 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+;; '(elfeed-feeds (quote ("https://ag91.github.io/rss.xml")))
  '(package-selected-packages (quote (auto-compile use-package-ensure-system-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
